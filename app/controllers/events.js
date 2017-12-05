ce3308c4723acee72980ccac94249cc32d40d075
Steve Tyler <stevetyler.uk@gmail.com>
2017-12-05 20:12:50 +0000

    renamed components to events

--- app/controllers/events.js
+++ app/controllers/events.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  currentDate: Date.now()
+  currentDate: Date.now(),
+  days: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+  months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 });
