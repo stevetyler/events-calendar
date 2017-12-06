2618bae83cbcd190248519fb4c9125000e6d0fbd
Steve Tyler <stevetyler.uk@gmail.com>
2017-12-06 17:22:26 +0000

    updated event model

--- app/models/event.js
+++ app/models/event.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
+import attr from 'ember-data/attr';
 
 export default DS.Model.extend({
-
+  startDate: attr('date'),
+  endDate: attr('date'),
+  dayDuration: attr('number'),
+  description: attr('string')
 });
