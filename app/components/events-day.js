57e31e85f8d5b6745dab803dd664ad799c495998
Steve Tyler <mail@steve-tyler.co.uk>
2017-12-06 00:33:00 +0000

    implemented contextual components and updated styling

--- app/components/events-day.js
+++ app/components/events-day.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['day'],
+  click() {
+    this.toggleProperty('isAddEventVisible');
+    console.log('clicked!', this.get('isAddEventVisible'));
+  },
+  actions: {
+    addDescription() {
+
+    }
+  }
 });
