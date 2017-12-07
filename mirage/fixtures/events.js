df43406d95e313fff655df369466a71b524781a9
Steve Tyler <mail@steve-tyler.co.uk>
2017-12-07 23:45:17 +0000

    updated fixtures

--- mirage/fixtures/events.js
+++ mirage/fixtures/events.js
@@ -3,14 +3,22 @@ import moment from 'moment';
 export default [
   {
     id: 1,
-    startDate: moment().subtract(3, 'day'),
-    endDate: moment().subtract(3, 'day'),
+    startDate: moment().subtract(1, 'day'),
+    endDate: moment().subtract(1, 'day'),
     teacher: 'Mr Lutman',
     subject: '4A Maths',
     title: 'Exams'
   },
   {
     id: 2,
+    startDate: moment().subtract(2, 'day'),
+    endDate: moment().add(1, 'day'),
+    teacher: 'Mr Adams',
+    subject: '4D Chemistry',
+    title: 'Exams'
+  },
+  {
+    id: 5,
     startDate: moment().subtract(1, 'day'),
     endDate: moment().add(5, 'day'),
     teacher: 'Mrs Taylor',
@@ -18,11 +26,11 @@ export default [
     title: 'Exams'
   },
   {
-    id: 3,
+    id: 6,
     startDate: moment(),
     endDate: moment().add(5, 'day'),
     teacher: 'Mrs Smith',
-    subject: '5B Geography',
+    subject: '3C Geography',
     title: 'Field Trip'
   }
 ];
