5b5a3fdbaabffe4fe3f05a3da6b06ec09d2c45fa
Steve Tyler <mail@steve-tyler.co.uk>
2017-12-07 12:27:11 +0000

    added event model and events route

--- app/models/event.js
+++ app/models/event.js
@@ -4,6 +4,9 @@ import attr from 'ember-data/attr';
 export default DS.Model.extend({
   startDate: attr('date'),
   endDate: attr('date'),
-  dayDuration: attr('number'),
-  description: attr('string')
+  title: attr('string'),
+  teacher: attr('string'),
+  subject: attr('string'),
+
+  dayDuration: 3,
 });
