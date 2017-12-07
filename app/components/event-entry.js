2295e4022252e7fc2e17054a204ecea32d6ab500
Steve Tyler <mail@steve-tyler.co.uk>
2017-12-07 13:28:19 +0000

    implemented filteredEvents CP to display event only if startDate matches weekDate

--- app/components/event-entry.js
+++ app/components/event-entry.js
@@ -1,5 +1,14 @@
 import Ember from 'ember';
+import moment from 'moment';
+const { computed } = Ember;
 
 export default Ember.Component.extend({
-  classNames: ['event-entry']
+  classNames: ['event-entry'],
+  filteredEvents: computed('events.[]', function() {
+    return this.get('events').filter(e => {
+      const formatCurrent = moment(e.get('startDate')).format('YYYY-MM-DD');
+      const formatDate = moment(this.get('date')).format('YYYY-MM-DD');
+      return formatCurrent === formatDate && !e.get('isNew');
+    });
+  })
 });
