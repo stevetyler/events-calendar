aa4b811b5822fa75e153c0723d5408764c435327
Steve Tyler <mail@steve-tyler.co.uk>
2017-12-07 23:35:30 +0000

    added isCurrentDay CP on events-day component

--- app/components/events-day.js
+++ app/components/events-day.js
@@ -1,14 +1,16 @@
 import Ember from 'ember';
+import moment from 'moment';
+const { computed } = Ember;
 
 export default Ember.Component.extend({
+  classNameBindings: ['isCurrentDay'],
   classNames: ['day'],
-  click() {
-    this.toggleProperty('isAddEventVisible');
-    console.log('clicked!', this.get('isAddEventVisible'));
-  },
-  actions: {
-    addDescription() {
+  mouseEnter() {},
+  mouseLeave() {},
+  isCurrentDay: computed('date', function() {
+    let now = moment().format('YYYY-MM-DD');
+    let date = moment(this.get('date')).format('YYYY-MM-DD');
 
-    }
-  }
+    return now === date ? true : false;
+  })
 });
