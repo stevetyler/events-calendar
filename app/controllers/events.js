9d579c52aad1e19edbfdcef0ad48b896762b8d15
Steve Tyler <mail@steve-tyler.co.uk>
2017-12-07 12:52:55 +0000

    added startDate and endDate properties and weekDates CP to controller

--- app/controllers/events.js
+++ app/controllers/events.js
@@ -1,8 +1,21 @@
 import Ember from 'ember';
+import moment from 'moment';
+const { computed } = Ember;
 
 export default Ember.Controller.extend({
   currentDate: Date.now(),
-  dayNames: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-  monthNames: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  startDate: moment().isoWeekday(1),
+  endDate: moment().isoWeekday(7),
+
+  weekDates: computed('startDate', 'endDate', function() {
+    let dateArray = [];
+    let startDate = this.get('startDate');
+    let endDate = this.get('endDate');
+    while (startDate <= endDate) {
+      dateArray.push( moment(startDate).format('YYYY-MM-DD') );
+      startDate = moment(startDate).add(1, 'days');
+    }
+    return dateArray;
+  }),
   isCreateEventVisible: false
 });
