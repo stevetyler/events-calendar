7320dcde6c605adcd17cf1de47c4d2530763421d
Steve Tyler <stevetyler.uk@gmail.com>
2017-12-08 12:38:34 +0000

    updated fixtures

--- mirage/fixtures/events.js
+++ mirage/fixtures/events.js
@@ -3,34 +3,50 @@ import moment from 'moment';
 export default [
   {
     id: 1,
-    startDate: moment().subtract(1, 'day'),
-    endDate: moment().subtract(1, 'day'),
-    teacher: 'Mr Lutman',
-    subject: '4A Maths',
+    startDate: moment(),
+    endDate: moment().add(1, 'day'),
+    teacher: 'Mr Perkins',
+    subject: '1A Chemistry',
     title: 'Exams'
   },
   {
     id: 2,
-    startDate: moment().subtract(2, 'day'),
-    endDate: moment().add(1, 'day'),
+    startDate: moment(),
+    endDate: moment().add(3, 'day'),
+    teacher: 'Mrs Smith',
+    subject: '3C Geography',
+    title: 'Field Trip'
+  },
+  {
+    id: 3,
+    startDate: moment().add(2, 'day'),
+    endDate: moment().add(5, 'day'),
     teacher: 'Mr Adams',
     subject: '4D Chemistry',
     title: 'Exams'
   },
   {
+    id: 4,
+    startDate: moment().add(4, 'day'),
+    endDate: moment().add(5, 'day'),
+    teacher: 'Mr David',
+    subject: '2F Geography',
+    title: 'Field Trip'
+  },
+  {
     id: 5,
     startDate: moment().subtract(1, 'day'),
-    endDate: moment().add(5, 'day'),
+    endDate: moment().add(4, 'day'),
     teacher: 'Mrs Taylor',
     subject: '5B English',
     title: 'Exams'
   },
   {
     id: 6,
-    startDate: moment(),
-    endDate: moment().add(5, 'day'),
-    teacher: 'Mrs Smith',
-    subject: '3C Geography',
-    title: 'Field Trip'
+    startDate: moment().subtract(3, 'day'),
+    endDate: moment().add(3, 'day'),
+    teacher: 'Mr Lutman',
+    subject: '5B Maths',
+    title: 'Exams'
   }
 ];
