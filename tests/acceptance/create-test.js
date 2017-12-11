4513831efcb86330fd87029431a9607d55bc8eec
Steve Tyler <mail@steve-tyler.co.uk>
2017-12-11 21:50:28 +0000

    tested week dates are rendered correctly

--- tests/acceptance/create-test.js
+++ tests/acceptance/create-test.js
@@ -1,10 +1,10 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'events-calendar/tests/helpers/module-for-acceptance';
-//import moment from 'moment';
+import moment from 'moment';
 
 moduleForAcceptance('Acceptance | create');
 
-test('visiting /create-test', function(assert) {
+test('visiting /events', function(assert) {
   // let mondaysEvent = server.create('event', {
   //   startDate: moment().isoWeekday(1),
   //   endDate: moment().isoWeekday(1).add(2, 'days'),
@@ -19,17 +19,21 @@ test('visiting /create-test', function(assert) {
   //   subject: '1B Physics',
   //   title: 'Exams'
   // });
-  //
-  // let weekDates = [];
-  // for (let i = 1; i <= 7; i++) {
-  //   weekDates.push(moment().isoWeekday(i));
-  // }
+
+  let weekDates = [];
+  for (let i = 1; i <= 7; i++) {
+    weekDates.push(moment().isoWeekday(i));
+  }
 
   visit('/events');
 
   andThen(() => {
     assert.equal(currentURL(), '/events');
     assert.equal(find('h1').text().trim(), 'Events Calendar', 'Correct title');
-  });
 
+    weekDates.forEach((date, i) => {
+      assert.equal(find(`.week .day:eq(${i}) .day-title h3:eq(0)`).text().trim(), date.format('dddd'), 'correct day in words');
+      assert.equal(find(`.week .day:eq(${i}) .day-title h3:eq(1)`).text().trim(), date.format('Do MMM'), 'correct day in ');
+    });
+  });
 });
